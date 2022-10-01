b808644186628a54d62a269de62dd8ad4bffaa72
kushan2k <kushangayantha001@gmail.com>
2022-10-01 17:52:01 +0530

    gap problem fixed

--- Components/Navbar.js
+++ Components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar({setcolor}) {
   return (
     <nav className='p-1.5 bg-gray-100 items-center flex h-16 px-4 justify-between mb-10'>
       <h1 className='invisible sm:visible text-xl text-center w-2/4'>Welcome</h1>
-      <ul className=' flex flex-row justify-evenly h-full items-center gap-10'>
+      <ul className=' flex flex-row justify-evenly gap-4 h-full items-center md:gap-10'>
         <li className=' mx-3'>
           
           <BsFillMoonStarsFill onProgress={()=>{setcolor()}} className='cursor-pointer text-xl'/>
